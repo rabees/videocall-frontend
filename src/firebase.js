dd9e843532e726ff4ed9ec4a0315670ada0caf59
Rabees <k190316@nu.edu.pk>
2023-07-04 22:47:45 +0500

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDuoPSbwsGagI6PyZTMnDikc6LveBOnNVw",
+    apiKey: process.env.apiKey,
     authDomain: "debud-mern.firebaseapp.com",
     projectId: "debud-mern",
     storageBucket: "debud-mern.appspot.com",
